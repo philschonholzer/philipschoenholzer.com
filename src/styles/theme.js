328f307453eda028a4ddfc26939fe0e341c7abf2
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2020-04-19 18:06:28 +0200

    Nicer background color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,9 @@
 const theme = {
   primary: 'hsla(262, 43%, 43%)',
-  primaryLight: 'hsla(262,43%,68%)',
-  background: 'hsl(0, 0%, 12%)',
-  backgroundLight: 'hsl(0, 0%, 20%)',
-  text: 'hsl(0,0%,100%)',
+  primaryLight: 'hsla(262, 43%, 68%)',
+  background: 'hsla(262, 8%, 12%)',
+  backgroundLight: 'hsla(262, 8%, 20%)',
+  text: 'hsl(0, 0%, 100%)',
   textDark: 'hsl(0, 0%, 70%)',
 }
 
