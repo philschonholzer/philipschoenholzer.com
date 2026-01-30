029ee80533d1279331434fec3f5f587aa04a5040
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-01-30 17:27:44 +0100

    Use biome for formatting and linting

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,5 +13,5 @@ export default defineConfig(
     },
     site: 'https://philipschoenholzer.com/',
     integrations: [icon()],
-  }
+  },
 )
--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test('homepage has title and links to appointment page', async ({
   page,
